df93e926c9d9bd05b48600df2c7c9883ec8e00b8
melanianf <melania.n@mail.ugm.ac.id>
2019-05-07 10:38:25 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,62 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
-import PrimarySearchAppBar from "./PrimarySearchAppBar";
-import HotelCard from "./HotelCard";
+import Main from './components/page/Main';
+import MyAppBar from "./components/MyAppBar";
 
-import Smap from "./SampleMap";
 class App extends Component {
-    state = {
-        open: false,
-        hotels: [
-            {nama: 'Lorem Ipsum', tanggal: '12 September 2019', deskrpsi: 'Lorem ipsum sit dolor amet'},
-            {nama: 'Lorem Ipsum', tanggal: '12 September 2019', deskrpsi: 'Lorem ipsum sit dolor amet'},
-            {nama: 'Lorem Ipsum', tanggal: '12 September 2019', deskrpsi: 'Lorem ipsum sit dolor amet'},
-            {nama: 'Lorem Ipsum',favorite : 'default', tanggal: '12 September 2019', deskrpsi: 'Lorem ipsum sit dolor amet'},
-        ]
-    };
-
-    handleClickOpen = () => {
-        this.setState({open: true});
-    };
-
-    handleClose = () => {
-        this.setState({open: false});
-    };
-
-    handleFavorite = (key) => {
-        if (this.state.hotels[key].favorite === 'secondary'){
-            this.setState.hotels[key].favorite = 'default';
-            this.setState({hotels : this.state.hotels});
-        }else{
-            this.setState.hotels[key].favorite = 'secondary';
-            this.setState({hotels : this.state.hotels});
-        }
-        console.log(this.state.hotels[key].favorite);
-    }
-
     render() {
         return (
-            <div className="row">
-                <div className="col m12">
-                    <PrimarySearchAppBar/>
-                    <br/>
-                    <Smap/>
-                    <div className="row">
-                        <div className="col m5">
-                            {this.state.hotels.map((data, key) => {
-                                return (
-                                    <div key={key}>
-                                        <HotelCard favoriteClicked={() => this.handleFavorite(key)} btnColor={this.state.favorite} title={data.nama} tanggal={data.tanggal} deskripsi={data.deskrpsi}/>
-                                        <br/>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                        {/* <div className="col m6">
-                            <h1>MAPS HERE</h1>
-                        </div> */}
-                    </div>
-                </div>
+            <div>
+                <MyAppBar/>
+                <Main/>
             </div>
         )
     }
---
+++ src/components/page/Portofolio.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Portofolio extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Portofolio Page</h1>
+            </div>
+        )
+    }
+}
+
+export default Portofolio;
